fbe81f902e0c04b63e5ea5bc01c0285b36cbff5c
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-01-31 13:13:33 +0100

    Cleaned up password trial + removed pwd font + fixed icon test/init

--- kaskadi-textbox.js
+++ kaskadi-textbox.js
@@ -7,7 +7,7 @@ class KaskadiTextbox extends KaskadiElement {
     super()
     this.labelHidden = false
     this.lang = 'en'
-    this.icon = null
+    this.icon = ''
     this.lastValueFired = ''
   }
 
@@ -82,7 +82,7 @@ class KaskadiTextbox extends KaskadiElement {
     return html`
       <div id="outer">
         <div id="start_label" class=" ${this.labelHidden ? 'hidden' : ''}" >
-          <div id="icon">${this.icon ? html`<img src="${this.icon}" height="20px" width="20px"/>` : ''}</div>
+          <div id="icon">${this.icon.length > 0 ? html`<img src="${this.icon}" height="20px" width="20px"/>` : html``}</div>
           <div id="label_text">${translate(this.label, this.lang)}</div>
         </div>
         <div id="text" contentEditable="true" @blur="${this.blur}" @paste="${this.paste}" @keydown="${this.keydown}"></div>
